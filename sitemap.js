b1012f44d8beb7f900271a9693d7f1c794c1c628
Dieffer <37056584+Dieffer@users.noreply.github.com>
2019-12-22 18:50:25 +0800

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -1,10 +1,10 @@
-<script>
 /**
  * Blogger Tabbed Style Table of Content Widget by Taufik Nurrohman
  * Free for change but keep the original attribution.
  * URL: https://plus.google.com/108949996304093815163/about
  * TEMPLATES: &lt;div id="tabbed-toc"&gt;&lt;span class="loading"&gt;Loading...&lt;/span&gt;&lt;/div&gt;&lt;script type="text/javascript"&gt;var tabbedTOC = {blogUrl:"http://latitudu.blogspot.com", containerId: "tabbed-toc", activeTab: 1};&lt;/script&gt;&lt;script type="text/javascript" src="js/tabbed-toc.js"&gt;&lt;/script&gt;
  */
+
 var tabbedTOC_defaults = {
 	blogUrl: "http://www.dte.web.id", // Blog URL
 	containerId: "tabbed-toc", // Container ID
@@ -16,7 +16,18 @@ var tabbedTOC_defaults = {
 	thumbSize: 40, // Thumbnail size
 	noThumb: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAA3NCSVQICAjb4U/gAAAADElEQVQImWOor68HAAL+AX7vOF2TAAAAAElFTkSuQmCC", // No thumbnail URL
 	monthNames: [ // Array of month names
-		"Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"
+		"Januari",
+		"Februari",
+		"Maret",
+		"April",
+		"Mei",
+		"Juni",
+		"Juli",
+		"Agustus",
+		"September",
+		"Oktober",
+		"November",
+		"Desember"
 	],
 	newTabLink: true, // Open link in new window?
 	maxResults: 99999, // Maximum posts result
@@ -25,6 +36,7 @@ var tabbedTOC_defaults = {
 	showNew: false, // `false` to hide the "New!" mark in most recent posts, or define how many recent posts are to be marked
 	newText: " - <em style='color:red;'>Baru!</em>" // HTML for the "New!" text
 };
+
 for (var i in tabbedTOC_defaults) {
 	tabbedTOC_defaults[i] = (typeof(tabbedTOC[i]) !== undefined && typeof(tabbedTOC[i]) !== 'undefined') ? tabbedTOC[i] : tabbedTOC_defaults[i];
 }
@@ -45,33 +57,38 @@ function clickTab(pos) {
 }
 
 function showTabs(json) {
-	var total = parseInt(json.feed.openSearch$totalResults.$t, 10),
+
+	var total = parseInt(json.feed.openSearch$totalResults.$t,10),
 		c = tabbedTOC_defaults,
 		entry = json.feed.entry,
 		category = json.feed.category,
 		skeleton = "",
 		newPosts = [];
+
 	for (var g = 0; g < (c.showNew === true ? 5 : c.showNew); g++) {
 		if (g == entry.length) break;
 		entry[g].title.$t = entry[g].title.$t + (c.showNew !== false ? c.newText : '');
 	}
-	entry = c.sortAlphabetically ? entry.sort(function(a, b) {
+
+	entry = c.sortAlphabetically ? entry.sort(function(a,b) {
 		return (a.title.$t.localeCompare(b.title.$t));
 	}) : entry;
-	category = c.sortAlphabetically ? category.sort(function(a, b) {
+	category = c.sortAlphabetically ? category.sort(function(a,b) {
 		return (a.term.localeCompare(b.term));
 	}) : category;
+
 	// Build the tabs skeleton
 	skeleton = '<span class="divider-layer"></span><ul class="toc-tabs">';
 	for (var h = 0, cen = category.length; h < cen; h++) {
 		skeleton += '<li class="toc-tab-item-' + h + '"><a href="javascript:clickTab(' + h + ');">' + category[h].term + '</a></li>';
 	}
 	skeleton += '</ul>';
+
 	// Bulid the tabs contents skeleton
 	skeleton += '<div class="toc-content">';
 	for (var i = 0, cnt = category.length; i < cnt; i++) {
 		skeleton += '<ol class="panel" data-category="' + category[i].term + '"';
-		skeleton += (i != (c.activeTab - 1)) ? ' style="display:none;"' : '';
+		skeleton += (i != (c.activeTab-1)) ? ' style="display:none;"' : '';
 		skeleton += '>';
 		for (var j = 0; j < total; j++) {
 			if (j == entry.length) break;
@@ -79,10 +96,11 @@ function showTabs(json) {
 				pub = entries.published.$t, // Get the post date
 				month = c.monthNames, // Month array from the configuration
 				title = entries.title.$t, // Get the post title
-				summary = ("summary" in entries && c.showSummaries === true) ? entries.summary.$t.replace(/<br ?\/?>/g, " ").replace(/<.*?>/g, "").replace(/[<>]/g, "").substring(0, c.numChars) + '&hellip;' : '', // Get the post summary
-				img = ("media$thumbnail" in entries && c.showThumbnails === true) ? '<img class="thumbnail" style="width:' + c.thumbSize + 'px;height:' + c.thumbSize + 'px;" alt="" src="' + entries.media$thumbnail.url.replace(/\/s72(\-c)?\//, "/s" + c.thumbSize + "-c/") + '"/>' : '<img class="thumbnail" style="width:' + c.thumbSize + 'px;height:' + c.thumbSize + 'px;" alt="" src="' + c.noThumb.replace(/\/s72(\-c)?\//, "/s" + c.thumbSize + "-c/") + '"/>', // Get the post thumbnail
+				summary = ("summary" in entries && c.showSummaries === true) ? entries.summary.$t.replace(/<br ?\/?>/g," ").replace(/<.*?>/g,"").replace(/[<>]/g,"").substring(0,c.numChars) + '&hellip;' : '', // Get the post summary
+				img = ("media$thumbnail" in entries && c.showThumbnails === true) ? '<img class="thumbnail" style="width:'+c.thumbSize+'px;height:'+c.thumbSize+'px;" alt="" src="' + entries.media$thumbnail.url.replace(/\/s72(\-c)?\//,"/s"+c.thumbSize+"-c/") + '"/>' : '<img class="thumbnail" style="width:'+c.thumbSize+'px;height:'+c.thumbSize+'px;" alt="" src="' + c.noThumb.replace(/\/s72(\-c)?\//,"/s"+c.thumbSize+"-c/") + '"/>', // Get the post thumbnail
 				cat = (entries.category) ? entries.category : [], // Post categories
-				date = (c.showDates) ? '<time datetime="' + pub + '" title="' + pub + '">' + pub.substring(8, 10) + ' ' + month[parseInt(pub.substring(5, 7), 10) - 1] + ' ' + pub.substring(0, 4) + '</time>' : ''; // Formated published date
+				date = (c.showDates) ? '<time datetime="' + pub + '" title="' + pub + '">' + pub.substring(8,10) + ' ' + month[parseInt(pub.substring(5,7),10)-1] + ' ' + pub.substring(0,4) + '</time>' : ''; // Formated published date
+				
 			for (var k = 0; k < entries.link.length; k++) {
 				if (entries.link[k].rel == 'alternate') {
 					link = entries.link[k].href; // Get the post URL
@@ -104,16 +122,19 @@ function showTabs(json) {
 		}
 		skeleton += '</ol>';
 	}
+
 	skeleton += '</div>';
 	skeleton += '<div style="clear:both;"></div>';
 	document.getElementById(c.containerId).innerHTML = skeleton;
-	clickTab(c.activeTab - 1);
+	clickTab(c.activeTab-1);
+
 }
+
 (function() {
 	var h = document.getElementsByTagName('head')[0],
 		s = document.createElement('script');
-	s.type = 'text/javascript';
-	s.src = tabbedTOC_defaults.blogUrl + '/feeds/posts/summary?alt=json-in-script&max-results=' + tabbedTOC_defaults.maxResults + '&orderby=published&callback=showTabs';
+		s.type = 'text/javascript';
+		s.src = tabbedTOC_defaults.blogUrl + '/feeds/posts/summary?alt=json-in-script&max-results=' + tabbedTOC_defaults.maxResults + '&orderby=published&callback=showTabs';
 	if (tabbedTOC_defaults.preload !== "onload") {
 		setTimeout(function() {
 			h.appendChild(s);
@@ -124,4 +145,3 @@ function showTabs(json) {
 		};
 	}
 })();
-</script>
